bbedb8bb7d4f5153e293f2e690fcc32b50015f70
maged6 <magedabdalmashi66@gmail.com>
2025-11-26 14:13:21 +0200

    add single section

---
+++ app/components/services-training-list/SecoundSingleTraining.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="py-[24px] px-[60px]">
+        <div class="flex justify-between pb-[24px] border-b-2 border-gray-200">
+            <div class="flex flex-col gap-[8px]">
+     <p class="text-[32px] font-[900] text-primary-10">
+              {{ titleSection }}
+            </p>
+                  <p class="text-[20px] font-[900] text-primary-20">{{ priceSection }} EGP</p>
+            </div>
+
+            <div>
+                      <MainBtn :title="'Register Now'" />
+            </div>
+       
+        </div>
+        <div class="py-[24px]">
+              <FiltersGroup
+        :filters="filters"
+        :selectedFilter="selectedFilter"
+        @update:selectedFilter="selectedFilter = $event"
+      />
+</div>
+
+    </section>
+</template>
+
+
+<script lang="ts">
+import MainBtn from '~/components/buttons/MainBtn.vue';
+import PriceAndOffer from '../main-component/PriceAndOffer.vue';
+import FiltersGroup from '../main-component/FiltersGroup.vue';
+
+export default {
+    name: 'SecoundSingleTraining',
+    components: { MainBtn, PriceAndOffer, FiltersGroup },
+    props: {
+        titleSection: { type: String, default: 'Compensation, Benefits and Incentives' },
+        priceSection: { type: String, default: '9500' },
+    },
+     data() {
+    return {
+      filters: [
+        { label: "Overview", value: "overview" },
+        { label: "Content", value: "content" },
+        { label: "Benefits", value: "benefits" },
+        { label: "Audience", value: "audience" },
+        { label: "Registration Terms", value: "registration" },
+        { label: "FAQs", value: "faqs" },
+      ],
+      selectedFilter: "overview",
+    };
+  },
+}
+</script>
--- app/pages/services/training-programs/[id].vue
+++ app/pages/services/training-programs/[id].vue
@@ -2,14 +2,17 @@
 <template>
   <MainSingleTraining
   />
+  <SecoundSingleTraining /> 
 </template>
 
 <script>
 import MainSingleTraining from "~/components/services-training-list/MainSingleTraining.vue";
+import SecoundSingleTraining from "~/components/services-training-list/SecoundSingleTraining.vue";
 
 export default {
   components: {
     MainSingleTraining,
+    SecoundSingleTraining,
   },
 };
 </script>
