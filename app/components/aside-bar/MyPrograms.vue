b0b880ffe4e6b03904550a4a7e02f74bc088ec35
maged6 <magedabdalmashi66@gmail.com>
2025-11-29 18:44:56 +0200

    add My Programs page

--- app/components/aside-bar/MyPrograms.vue
+++ app/components/aside-bar/MyPrograms.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        My Programs aaaaaaaaaaaaa
-    </div>
+    <section>
+        <div class="flex flex-col w-[70vw]">
+        <div class="pb-[16px]">
+        <BreadcrumbComponent 
+        :breadcrumbArray="breadcrumbArray" 
+        :fontSize="12" 
+        :textColor="'gray-600'"/>
+        </div>
+        <div class="flex justify-between">
+            <h1 class="font-[900] text-[32px] text-primary-10">My Programs</h1>
+            <div>
+                <MainBtn :title="'Register New Program'"/>
+            </div>
+        </div>
+
+        <div class="pt-[32px]">
+           <MyProgramsCard />
+        </div>
+        </div>
+    </section>
 </template>
+
+
+<script lang="ts">
+
+import { defineComponent, type PropType } from 'vue'
+import BreadcrumbComponent from "../breadcrumb/Breadcrumb.vue";
+import MainBtn from '../buttons/MainBtn.vue';
+import MyProgramsCard from '../cards/MyProgramsCard.vue';
+
+
+export default defineComponent({
+  components: {
+    BreadcrumbComponent,
+    MainBtn,
+    MyProgramsCard
+  },
+
+  data() {
+    return {
+        breadcrumbArray: [
+                { name: 'Home', to: '/' },
+                { name: 'My Programs', to: '/my-account' }
+            ]
+
+    }
+  }
+})
+</script>
---
+++ app/components/cards/MyProgramsCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div
+    class="flex justify-center 
+     items-stretch  w-full min-h-[262px]"
+  >
+    <div class="max-w-[341px]">
+      <NuxtImg
+        class="rounded-l-[16px] w-full h-full object-cover"
+        :src="'images/layout-hero.jpg'"
+        alt="the training hub image"
+        loading="lazy"
+        format="webp"
+        width="350"
+        height="200"
+      />
+    </div>
+
+    <div
+      class="border border-gray-200 shadow-xs rounded-r-[16px] bg-white p-[28px] w-full flex justify-between"
+    >
+      <div class="flex flex-col justify-between items-start h-full">
+        <div>
+          <p
+            class="bg-tertiary-gray-10 rounded-[4px] px-4 py-1 font-[900] w-fit"
+          >
+            Soft Skills
+          </p>
+          <p class="font-[900] text-[20px] pt-[8px]">
+            Compensation, Benefits and Incentives
+          </p>
+        </div>
+
+        <div class="flex flex-col gap-[8px]">
+          <p class="font-[100] text-[14px] text-gray-900">
+            2 Days/ Week .6 Weeks
+          </p>
+          <p class="font-[100] text-[14px] text-gray-900">
+            Start From 25th Oct 2025
+          </p>
+          <p class="font-[900] text-[20px] text-primary-20">8,000 EGP</p>
+        </div>
+      </div>
+      <div class="flex flex-col justify-between items-end h-full">
+        <p class="bg-tertiary-gray-10 rounded-[4px] px-4 py-1 font-[900] w-fit">
+          Active
+        </p>
+        <div class="flex items-end">
+          <ArrowRightTop
+            :backgroundColorIcon="'#b22726'"
+            :colorIcon="'white'"
+            :width="'42'"
+            :height="'42'"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import ArrowRightTop from '~/components/svg/ArrowRightTopBtn.vue'
+
+export default {
+  components: { ArrowRightTop },
+
+  props: {
+
+  }
+};
+</script>
